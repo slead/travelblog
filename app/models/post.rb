6ced3f7c72b78103e7e8b3c74a8f85b191fc9428
Stephen Lead <stephen.lead@gmail.com>
2017-10-13 11:12:17 +1100

    Add geocoder to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ApplicationRecord
 	include FriendlyId
 	friendly_id :title, use: :slugged
+
+	geocoded_by :placename
+	reverse_geocoded_by :latitude, :longitude do |obj, results|
+    if geo = results.first
+      obj.city = geo.city
+      obj.state = geo.state
+      obj.country = geo.country
+    end
+  end
+  after_validation :geocode, if: :placename_changed?
+  after_validation :reverse_geocode, if: :placename_changed?
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012231323) do
+ActiveRecord::Schema.define(version: 20171013000233) do
 
   create_table "blahs", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20171012231323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "placename"
   end
 
 end
