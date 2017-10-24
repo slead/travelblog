543eb4ea7d945379d0de00c0811bfb3821ac4b77
Stephen Lead <stephen.lead@gmail.com>
2017-10-25 10:39:24 +1100

    require login to manipulate photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /photos
   # GET /photos.json
