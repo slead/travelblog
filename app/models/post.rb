79e64a0295466088641eac30dc3fe32e58b7c043
Stephen Lead <stephen.lead@gmail.com>
2017-10-24 22:14:09 +1100

    Move the photo checking to the Post model
    Ensures the photos are checked on create, not just edit

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,11 +15,45 @@ class Post < ApplicationRecord
       self.published_date = self.created_at
     end
 
-    # Put validate photos routine here
-    if self.flickr_album.present?
-      
+    # Attach Flickr photos to this post if specified as flickr_album
+    begin
+      if self.flickr_album.present?
+        photoset_id = self.flickr_album
+        flickr.photosets.getPhotos(photoset_id: photoset_id).photo.map do |photo|
+          photo_id = photo['id']
+          if (Photo.where(:flickr_id => photo_id).count == 0)
+            photo_info = flickr.photos.getInfo :photo_id => photo_id
+            date_taken = photo_info['dates']['taken']
+            title = photo_info['title']
+            description = photo_info['description']
+            photo_sizes = flickr.photos.getSizes :photo_id => photo_id
+            thumb = ''
+            small = ''
+            medium = ''
+            large = ''
+            photo_sizes.each do |size|
+              label = size['label']
+              if label == 'Thumbnail'
+                thumb = size['source']
+              elsif label == 'Small'
+                small = size['source']
+              elsif label == 'Medium'
+                medium = size['source']
+              elsif label == 'Large'
+                large = size['source']
+              end
+            end
+            photo = Photo.create!(flickr_id: photo_id, title: title, description: description, date_taken: date_taken, thumb: thumb, small: small, medium: medium, large: large)
+          else
+            photo = Photo.where(:flickr_id => photo_id).first
+          end
+          if not self.photos.exists?(photo.id)
+            self.photos << photo
+          end
+        end      
+      end
+    rescue
     end
-
   end
 
 	geocoded_by :placename
