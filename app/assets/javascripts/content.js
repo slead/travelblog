c1201ddb9cdfa3cd9141a598cc0332f011a956f9
Stephen Lead <stephen.lead@gmail.com>
2017-10-26 12:55:41 +1100

    remove zoom control on mobile devices

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -4,6 +4,11 @@ ready = function() {
 
 	if ( $("#map").length ) {
 
+    var zoomControl = true;
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+     zoomControl = false;
+    }
+
 		app.basemap = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 			subdomains: 'abcd',
@@ -23,7 +28,8 @@ ready = function() {
 	    maxZoom: 6,
 	    minZoom: 2,
 	    layers: [app.basemap],
-	    maxBounds: L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180))
+	    maxBounds: L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180)),
+      zoomControl: zoomControl
 	  });
 	
     // Fetch geocoded posts
