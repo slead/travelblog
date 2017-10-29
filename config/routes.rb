66747b6cf55a6ab8a1ff8cd742e5d5f5a5f7b6ff
Stephen Lead <stephen.lead@gmail.com>
2017-10-29 20:56:48 +1100

    Change sign_up to subscribe

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/about', to: 'content#about', as: :about #=> about_path 
+  get '/about', to: 'content#about', as: :about #=> about_path
   get '/map', to: 'content#map', as: :map #=> map_path
-  get '/signup', to: 'content#signup', as: :signup #=> signup_path
+  get '/subscribe', to: 'content#subscribe', as: :subscribe #=> subscribe_path
   get '/:id', to: 'posts#show', as: :post_path
-  
+
   root to: "posts#index"
 end
