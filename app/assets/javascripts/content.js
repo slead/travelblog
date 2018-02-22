c2a7193026d2ff00c42311aba37c9671cfc235e9
Stephen Lead <stephen.lead@gmail.com>
2018-02-22 22:49:33 +0100

    Remove maptiks

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -4,8 +4,6 @@ ready = function() {
 
 	if ( $("#map").length ) {
 
-    maptiks.trackcode='fa969613-9010-44fb-b866-1750026fcdb3';
-
     var zoomControl = true;
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
      zoomControl = false;
