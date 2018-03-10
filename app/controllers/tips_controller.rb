aee8ebbd538b3c78140716c2ef0295f158f925e0
Stephen Lead <stephen.lead@gmail.com>
2018-03-10 21:53:16 +0100

    Simple change to try forcing Heroku to update Tips controller

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -10,9 +10,6 @@ class TipsController < ApplicationController
   end
 
   def index
-    # if params[:query].present?
-    #   # Find tips using elastic search
-    #   @tips = Tip.search(params[:query], page: params[:page], :per_page => 10)
     if params[:id].present?
       @tip = Tip.find(params[:id])
       redirect_to @tip
