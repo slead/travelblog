6da486494383caeda0f5496692f367a1ad0169ed
Stephen Lead <stephen.lead@gmail.com>
2018-10-14 18:33:38 -0300

    Remove the map JSON section from the posts controller
    now handled by the GetPins controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,45 +13,24 @@ class PostsController < ApplicationController
     # if params[:query].present?
     #   # Find posts using elastic search
     #   @posts = Post.where("status = 'published'").search(params[:query], page: params[:page], :per_page => 10)
-    if params[:map].present?
-      @posts = Post.where(["latitude IS NOT NULL and status= ?", "published"]).order published_date: :desc
-    elsif params[:id].present?
-      # This path is called when the user chooses an Ignite from the dropdown on the Posts page. In
-      # this case, open that post's homepage (and break out of the rest of this function)
+    if params[:id].present?
       @post = Post.find(params[:id])
       redirect_to @post
       return
     else
       @posts = Post.paginate(:page => params[:page], :per_page => 4).order published_date: :desc
     end
+
     begin
       @header_image_url = @posts.where(status: "published").first.photos.order(:sort).first.large
     rescue
       @header_image_url = 'https://farm9.staticflickr.com/8026/7254508562_25fc4962e5_b.jpg'
     end
 
-    # Make a JSON object from the posts, to add to the map
-    @geojson = Array.new
-
-    @posts.each do |post|
-      post.photos.count > 0 ? photo = post.photos.order(:sort).first.small : photo = nil
-      @geojson << {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [post.longitude, post.latitude]
-        },
-        properties: {
-          title: post.title,
-          url: "/posts/" + post.slug,
-          photo: photo
-        }
-      }
-    end
     respond_to do |format|
       format.html
       # Format the response for the map
-      format.json { render json: @geojson }
+      format.json
     end
   end
 
