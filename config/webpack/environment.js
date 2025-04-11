3df67b2069fc2964bc8eee36f16415a23e8f06d1
Stephen.Lead <Stephen.Lead@transport.nsw.gov.au>
2025-04-11 22:21:04 +0100

    pre-compile the NAV changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,20 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+// Remove any optimization configuration
+environment.config.delete("optimization");
+
+// Update module rules to remove sideEffects
+const babelLoader = environment.loaders.get("babel");
+if (babelLoader) {
+  babelLoader.use[0].options = {
+    ...babelLoader.use[0].options,
+    presets: ["env"],
+    plugins: [
+      "syntax-dynamic-import",
+      "transform-class-properties",
+      "transform-object-rest-spread",
+    ],
+  };
+}
+
+module.exports = environment;
